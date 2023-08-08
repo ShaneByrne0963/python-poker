7e3ff285efb79b8f5933a5df613a68c1f398a94c
ShaneByrne0963 <>
2023-08-08 11:43:06 +0000

    add: convert_hand which finds numbers in hand input for card ranks

--- run.py
+++ run.py
@@ -1,5 +1,41 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+def convert_hand(cards_list):
+    """
+    Converts a list of strings into a list of objects
+    containing the rank and the suit
+    Example: "10 of Diamonds" => {'rank': 10, 'suit': 'Diamonds'}
+    """
+    new_cards = []
+    for card in cards_list:
+        rank = get_rank(card)
+        if rank is not None:
+            new_cards.append({'rank': rank, 'suit': ''})
+        else:
+            return None
+    return new_cards
+
+
+def get_rank(card):
+    """
+    Gets the rank of a card from a given string
+    """
+    # The first rank the algorithm finds.
+    # If multiple ranks are found then a ValueError will be raised
+    found_rank = None
+    try:
+        # Checking if the rank of the card is a number (2-10)
+        for number in range(2, 11):
+            number_str = str(number)
+            if number_str in card:
+                found_rank = number
+    except ValueError as e:
+        print(f'Invalid input: {e}. Please try again.\n')
+        return None
+    else:
+        return found_rank
+
+
 def validate_hand(cards_list):
     """
     Checks if a poker hand is valid
@@ -15,6 +51,7 @@ def validate_hand(cards_list):
         print(f'Invalid input: {e}. Please try again.\n')
         return None
     else:
+        formatted_hand = convert_hand(cards_list)
         return formatted_hand
 
 
@@ -22,9 +59,12 @@ def get_hand_input():
     """
     Requests a hand to be manually entered by the user
     """
+    # Keep requesting an input from the user until a valid hand is entered
     while True:
         print('Please enter your poker hand.')
-        print('- Your hand must contain at least 5 cards, separated by a comma.')
+        print(
+            '- Your hand must contain at least 5 cards, separated by a comma.'
+        )
         print('- Each card must clearly indicate its rank and its suit.')
         print('- Example: "King of Hearts", "King Heart", "KH"\n')
         hand_input = input('Enter hand here: ')
